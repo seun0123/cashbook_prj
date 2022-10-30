a5a731b10bf9f14df036aee54e13b6dbcac8a6c8
seun0123 <seunpark04@gmail.com>
2022-10-30 11:29:59 +0900

    1030_comment

--- account/models.py
+++ account/models.py
@@ -10,6 +10,6 @@ class User(AbstractUser):
     phone = models.CharField(max_length=100, null=True, blank=True)
     address = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(upload_to = 'images/', blank=True)
-    def __str__(self):
-        return self.username
+    # def __str__(self):
+    #     return self.username
 
